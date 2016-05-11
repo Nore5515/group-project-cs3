458cce7e2b661f66864e839c2e2884a2b7ef0deb
Nore5515 <Nore5515@gmail.com>
2016-05-11 16:09:39 -0500

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -13,9 +13,17 @@ public class GUI {
 	int gridSize;
 	OurKeyListener gui_kList;
 	Player play;
-	List<Collider> colliders;
 	int xBuffer;
 	int yBuffer;
+	
+	
+	//List of all other lists of colliders
+	List<List<Collider>> colliders;
+	//item 0
+	List<Collider> walls;
+	//item 1
+	List<Collider> coins;
+
 	
 	public GUI(int _gridSize){
 		f = new JFrame();
@@ -26,12 +34,29 @@ public class GUI {
 		gridSize = _gridSize;
 		xBuffer = (f.getWidth())/(gridSize-1);
 		yBuffer = (f.getHeight())/(gridSize-1);
-		
-		colliders = new ArrayList<Collider>();
+
 		play = new Player(Math.round(gridSize/2),Math.round(gridSize/2));
 		gui_kList = new OurKeyListener(play, this, gridSize);
 
-		System.out.println("Work?");
+		
+		colliders = new ArrayList<List<Collider>>();
+		coins = new ArrayList<Collider>();
+		walls = new ArrayList<Collider>();
+		colliders.add(coins);
+		colliders.add(walls);
+		
+		//Adds Walls to List
+		for (int x = 0; x < gridSize; x++){
+			walls.add(new Wall(x*xBuffer, 0));
+			walls.add(new Wall(0, x*yBuffer));
+			walls.add(new Wall(x*xBuffer, yBuffer*(gridSize-3)));
+			walls.add(new Wall(xBuffer*(gridSize-2), x*yBuffer));
+		}
+		
+		//Adds Coins to List
+		coins.add(new Collectable(7*xBuffer, 7*yBuffer));
+		
+		
 		p = new JPanel(){
 			//Below is Panel Painting
 			public void paintComponent(Graphics g){
@@ -39,16 +64,6 @@ public class GUI {
 				
 				xBuffer = (f.getWidth())/(gridSize-1);
 				yBuffer = (f.getHeight())/(gridSize-1);
-
-				/** OBSOLETE
-				for (int x = 0; x < gridSize; x++){
-					for (int y = 0; y < gridSize; y++){
-						// Actually Unnecessary \/
-						g.drawLine(0,y*yBuffer,x*xBuffer,y*yBuffer);
-					}
-					g.drawLine(x*xBuffer, 0, x*xBuffer, f.getHeight());
-				}
-				 	OBSOLETE**/
 				
 				//New Graph Below
 				for (int x = 0; x < gridSize; x++){
@@ -60,24 +75,28 @@ public class GUI {
 				//New Graph Above
 				
 				
+				//Adds coins Below
+				g.setColor(Color.YELLOW);
+				for (int x = 0; x < coins.size(); x++){
+					System.out.println("Drawing Coin at " + coins.get(x).getX() + "," + coins.get(x).getY());
+					g.fillRect(coins.get(x).getX(), coins.get(x).getY(), xBuffer, yBuffer);
+				}
+				//Adds coins Above
+				
+				
 				//Player Below
 				g.setColor(Color.RED);
 				g.fillRect(play.getX()*xBuffer, play.getY()*yBuffer, xBuffer, yBuffer);
-				System.out.println("Player at" + (play.getX()*xBuffer) + "," + (play.getY()*yBuffer));
 				//Player Above
-				
+
 				
 				//Walls Below
 				g.setColor(Color.BLACK);
 				for (int x = 0; x < gridSize; x++){
 					g.fillRect(x*xBuffer, 0, xBuffer, yBuffer);
-					colliders.add(new Wall(x*xBuffer, 0));
 					g.fillRect(0, x*yBuffer, xBuffer, yBuffer);
-					colliders.add(new Wall(0, x*yBuffer));
 					g.fillRect(x*xBuffer, yBuffer*(gridSize-3), xBuffer, yBuffer);
-					colliders.add(new Wall(x*xBuffer, yBuffer*(gridSize-3)));
 					g.fillRect(xBuffer*(gridSize-2), x*yBuffer, xBuffer, yBuffer);
-					colliders.add(new Wall(xBuffer*(gridSize-2), x*yBuffer));
 				}
 				//Walls Above
 				
@@ -91,15 +110,19 @@ public class GUI {
 	public JFrame getFrame(){
 		return f;
 	}
-
+	
 	public void callRepaint() {
 		// TODO Auto-generated method stub
 		p.repaint();
 	}
-
-	public List<Collider> getColliders() {
+	
+	public List<List<Collider>> getColliders() {
 		// TODO Auto-generated method stub
 		return colliders;
+	}
+	
+	public void updateColliders(List<List<Collider>> _colliders){
+		colliders = _colliders;
 	}
 	
 	
